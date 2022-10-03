c98ca4b7507f15a8bac8d06b5d8ee4cae68bad69
Steffen Köhler <steffen@koehlers.de>
2022-10-03 09:34:34 +0200

    simCanBus with optional Baud rate as parameter

--- simCanBus.sh
+++ simCanBus.sh
@@ -1,4 +1,9 @@
 BAUDRATE=250000
+if [ ! -z "$1" ]
+  then
+    BAUDRATE="$1"
+fi
+echo "Use Baudrate of $BAUDRATE"
 sudo modprobe can
 sudo modprobe can_raw
 sudo modprobe can_bcm
@@ -9,4 +14,4 @@ sudo ip link set can0 type can bitrate $BAUDRATE
 sudo ip link set up can0
 sudo ip link set can1 type can bitrate $BAUDRATE
 sudo ip link set up can1
-
+cangen -v can0
