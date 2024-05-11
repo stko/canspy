0113778056203c45c973261a324b83c682bd2063
Steffen Köhler <steffen@koehlers.de>
2024-05-11 13:15:55 +0200

    now with local can id filtering

--- samples/ESPlay_Micro_V2_CircuitPython/code.py
+++ samples/ESPlay_Micro_V2_CircuitPython/code.py
@@ -2,7 +2,10 @@ from cpdui import CPDUI
 from espcan import ESPCan
 from csmqtt import CSMQTT
 from pcbbuttons import KeyPad
-
+import csutils
+status={
+    "msgs":{}
+}
 print("geht 1")
 cpdui=CPDUI()
 print("geht 2")
@@ -14,9 +17,15 @@ csmqtt=CSMQTT(None)
 print("geht 5")
 keypad=KeyPad(2)
 print("geht 6")
+defaults=csutils.load_defaults()
 while True:
     can.collect([])
     print(can.telegrams)
+    status["msgs"]=csutils.filter_telegrams(defaults,can.telegrams)
+    print("modules")
+    for module,data in status["msgs"].items():
+        print(module, data["state"])
+    csmqtt.send_topic(can.telegrams)
     csmqtt.handle_mqtt()
     while keypad.refresh():
         for index, pin in keypad.items():
--- samples/ESPlay_Micro_V2_CircuitPython/csmqtt.py
+++ samples/ESPlay_Micro_V2_CircuitPython/csmqtt.py
@@ -4,7 +4,9 @@ import ssl
 import socketpool
 import time
 import wifi
+import json
 import microcontroller
+import mdns
 import adafruit_minimqtt.adafruit_minimqtt as MQTT
 
 class CSMQTT:
@@ -15,12 +17,14 @@ class CSMQTT:
         self.on_message=on_message
         self.last_connect_trial_time=0
         self.mqtt_client=None
+        '''
         # calculate the device id
         s = microcontroller.cpu.uid
         self.device_id = ""
         for b in s:
             self.device_id += hex(b)[2:]
-
+        '''
+        self.device_id =mdns.Server(wifi.radio).hostname
         # Create a socket pool
         self.pool = socketpool.SocketPool(wifi.radio)
         self.ssl_context = ssl.create_default_context()
@@ -34,52 +38,58 @@ class CSMQTT:
         # ssl_context.load_cert_chain(
         #     certfile=os.getenv("device_cert_path"), keyfile=os.getenv("device_key_path")
         # )
-
+        # Set up a MiniMQTT Client
+        self.mqtt_client = MQTT.MQTT(
+            broker=os.getenv("MQTT_HOST"),
+            port=1883,
+            username=os.getenv("MQTT_USER"),
+            password=os.getenv("MQTT_PASSWORD"),
+            socket_pool=self.pool,
+            ssl_context=self.ssl_context,
+            client_id=self.device_id,
+        )
+        
+        # Setup the callback methods above
+        self.mqtt_client.on_connect = self.connected
+        self.mqtt_client.on_disconnect = self.disconnected
+        self.mqtt_client.on_message = self.message
+        
         
     def handle_mqtt(self):
         '''
          should be called periodically to handle the mqtt traffic
         '''
-        if self.mqtt_client==None and time.monotonic() - self.last_connect_trial_time > 10:
-            self.last_connect_trial_time = time.monotonic()
-            try:
-                # Set up a MiniMQTT Client
-                self.mqtt_client = MQTT.MQTT(
-                    broker=os.getenv("MQTT_HOST"),
-                    port=1883,
-                    username=os.getenv("MQTT_USER"),
-                    password=os.getenv("MQTT_PASSWORD"),
-                    socket_pool=self.pool,
-                    ssl_context=self.ssl_context,
-                )
-                
-                # Setup the callback methods above
-                self.mqtt_client.on_connect = connected
-                self.mqtt_client.on_disconnect = disconnected
-                self.mqtt_client.on_message = message
-                
-                # Connect the client to the MQTT broker.
-                print("Connecting to MQTT...")
-                mqtt_client.connect()
-            except:
-                print("No MQTT connection")
+        if not self.mqtt_client.is_connected():
+            if time.monotonic() - self.last_connect_trial_time > 10:
+                self.last_connect_trial_time = time.monotonic()
+                try:
+
+                    # Connect the client to the MQTT broker.
+                    print("Connecting to MQTT...")
+                    self.mqtt_client.connect()
+                except Exception as ex:
+                    print("No MQTT connection", str(ex))
+
                 
         else:
             # Poll the message queue
             try:
-                mqtt_client.loop(timeout=1)
-            except:
-                print("MQTT Poll error")
-
-        def send_topic(self,data):
+                self.mqtt_client.loop(timeout=1)
+            except Exception as ex:
+                print("MQTT Poll error",str(ex))
+                self.mqtt_client.reconnect()
                 
-                # Send a new message
-                try:
-                    print(f"Sending photocell value: {photocell_val}...")
-                    mqtt_client.publish(self.mqtt_publish, data)
-                    print("Sent!")
-                except:
-                    pass
+
+    def send_topic(self,data):
+            
+            # Send a new message
+            try:
+                print(f"Sending photocell value: {data}...")
+                self.mqtt_client.publish(self.mqtt_publish, json.dumps(data))
+                print("Sent!")
+            except Exception as ex:
+                print("MQTT Send error",str(ex))
+                self.mqtt_client.reconnect()
 
 
     # Define callback methods which are called when events occur
@@ -87,14 +97,13 @@ class CSMQTT:
     def connected(self,client, userdata, flags, rc):
         # This function will be called when the client is connected
         # successfully to the broker.
-        print("Connected to MQTT")
+        print("Connected to MQTT, subscripe to",self.mqtt_subscribe)
         # Subscribe to all changes on the onoff_feed.
         client.subscribe(self.mqtt_subscribe)
     
     
     def disconnected(self,client, userdata, rc):
         # This method is called when the client is disconnected
-        self.mqtt_client=None
         self.last_connect_trial_time = time.monotonic()
         print("Disconnected MQTT!")
     
