684d06c98e02da3cdec0009a30cd4142d6aab02a
Steffen Köhler <steffen@koehlers.de>
2024-05-13 08:10:02 +0200

    global try/except to reset controller on all uncatched exceptions

--- samples/ESPlay_Micro_V2_CircuitPython/code.py
+++ samples/ESPlay_Micro_V2_CircuitPython/code.py
@@ -4,6 +4,7 @@ from csmqtt import CSMQTT
 from pcbbuttons import KeyPad
 import csutils
 import json
+import microcontroller
 
 def on_topic(data):
     global defaults
@@ -13,34 +14,36 @@ def on_topic(data):
             defaults["modules"]=data["modules"]
     except Exception as ex:
         print("JSON error", ex,data)
-
-status={
-    "msgs":{}
-}
-print("geht 1")
-cpdui=CPDUI()
-print("geht 2")
-cpdui.draw()
-print("geht 3")
-can=ESPCan(0.9,10.0)
-print("geht 4")
-csmqtt=CSMQTT(on_topic)
-print("geht 5")
-keypad=KeyPad(2)
-print("geht 6")
-defaults=csutils.load_defaults()
-while True:
-    can.collect([])
-    print(can.telegrams)
-    status["msgs"]=csutils.filter_telegrams(defaults,can.telegrams)
-    print("modules")
-    for module,data in status["msgs"].items():
-        print(module, data["state"])
-    csmqtt.send_topic(can.telegrams)
-    csmqtt.handle_mqtt()
-    while keypad.refresh():
-        for index, pin in keypad.items():
-            print(index,pin.pin, pin.key_up, pin.key_down)
-
+try:
+    status={
+        "msgs":{}
+    }
+    print("geht 1")
+    cpdui=CPDUI()
+    print("geht 2")
+    cpdui.draw()
+    print("geht 3")
+    can=ESPCan(0.9,10.0)
+    print("geht 4")
+    csmqtt=CSMQTT(on_topic)
+    print("geht 5")
+    keypad=KeyPad(2)
+    print("geht 6")
+    defaults=csutils.load_defaults()
+    while True:
+        can.collect([])
+        print(can.telegrams)
+        status["msgs"]=csutils.filter_telegrams(defaults,can.telegrams)
+        print("modules")
+        for module,data in status["msgs"].items():
+            print(module, data["state"])
+        csmqtt.send_topic(can.telegrams)
+        csmqtt.handle_mqtt()
+        while keypad.refresh():
+            for index, pin in keypad.items():
+                print(index,pin.pin, pin.key_up, pin.key_down)
+except Exception as ex:
+    print("reset because of ", str(ex))
+    microcontroller.reset()
 
 
